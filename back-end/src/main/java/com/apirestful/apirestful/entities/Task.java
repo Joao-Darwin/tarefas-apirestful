d632094968b5d34a7fe4b745d2191e347c3818ae
João Darwin <joaodarwin.ip22@gmail.com>
2022-12-26 21:24:52 -0300

    Criação das Entidades

---
+++ back-end/src/main/java/com/apirestful/apirestful/entities/Task.java
@@ -0,0 +1,118 @@
+package com.apirestful.apirestful.entities;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.Objects;
+
+import com.apirestful.apirestful.enums.TaskStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_task")
+public class Task implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String description;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+	private Date date;
+
+	private Integer taskStatus;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public Task() {
+	}
+
+	public Task(Long id, String name, String description, Date date, TaskStatus status, User user) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.date = date;
+		setTaskStatus(status);
+		this.user = user;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public TaskStatus getStatus() {
+		return TaskStatus.valueOf(taskStatus);
+	}
+	
+	public void setTaskStatus(TaskStatus taskStatus) {
+		if(taskStatus != null) {
+			this.taskStatus = taskStatus.getCode();
+		}
+	}  
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Task other = (Task) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
