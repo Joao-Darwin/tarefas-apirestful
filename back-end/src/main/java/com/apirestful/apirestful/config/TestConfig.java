6847bce04f8bfb21383a3ba02823c0554760227c
João Darwin <joaodarwin.ip22@gmail.com>
2023-01-05 14:33:58 -0300

    Persistência HardCode na Entidade Task com TaskRepository

--- back-end/src/main/java/com/apirestful/apirestful/config/TestConfig.java
+++ back-end/src/main/java/com/apirestful/apirestful/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.apirestful.apirestful.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,15 +8,21 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.apirestful.apirestful.entities.Task;
 import com.apirestful.apirestful.entities.User;
+import com.apirestful.apirestful.enums.TaskStatus;
+import com.apirestful.apirestful.repositories.TaskRepository;
 import com.apirestful.apirestful.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner{
-
+	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private TaskRepository taskRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -23,6 +30,10 @@ public class TestConfig implements CommandLineRunner{
 		User u2 = new User(null, "Isabelly", "isabelly.ip23@gmail.com", "88 9 998753456", "isabelly@123");
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
+		
+		Task t1 = new Task(null, "Terminar fluxograma", "Fluxograma da tela de login", Instant.now(), TaskStatus.PENDING, u1);
+		
+		taskRepository.save(t1);
 	}
 
 	
