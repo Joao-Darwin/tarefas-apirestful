65ea3a91ec864ac2783fc5768c6af95420e384ee
João Darwin <joaodarwin.ip22@gmail.com>
2023-01-11 11:56:49 -0300

    Implementação do delete no UserResource

--- back-end/src/main/java/com/apirestful/apirestful/resources/UserResource.java
+++ back-end/src/main/java/com/apirestful/apirestful/resources/UserResource.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +39,16 @@ public class UserResource {
 	public ResponseEntity<List<User>> findAll() {
 		List<User> list = userService.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		userService.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user){
+		user = userSer
 	}
 }
