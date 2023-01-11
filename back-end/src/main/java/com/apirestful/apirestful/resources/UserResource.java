7e715c676e51b647b3381e6d5734005c77be9216
João Darwin <joaodarwin.ip22@gmail.com>
2023-01-11 11:26:04 -0300

    Implementação do insert no UserResource

---
+++ back-end/src/main/java/com/apirestful/apirestful/resources/UserResource.java
@@ -0,0 +1,25 @@
+package com.apirestful.apirestful.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apirestful.apirestful.entities.User;
+import com.apirestful.apirestful.services.UserService;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user) {
+		User nUser = userService.insert(user);
+		return ResponseEntity.ok().body(nUser);
+	}
+}
