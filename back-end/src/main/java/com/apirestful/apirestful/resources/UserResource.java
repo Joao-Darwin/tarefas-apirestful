4d49d2b6ef3f2b9a8d6510d79276decd1477fef1
João Darwin <joaodarwin.ip22@gmail.com>
2023-01-11 11:30:30 -0300

    Implementação do findById no UserResource

--- back-end/src/main/java/com/apirestful/apirestful/resources/UserResource.java
+++ back-end/src/main/java/com/apirestful/apirestful/resources/UserResource.java
@@ -2,6 +2,8 @@ package com.apirestful.apirestful.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,12 @@ public class UserResource {
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User user) {
 		User nUser = userService.insert(user);
+		return ResponseEntity.ok().body(nUser);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User nUser = userService.findByid(id);
 		return ResponseEntity.ok().body(nUser);
 	}
 }
