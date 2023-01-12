066cf3324bd1a940d2887db735a6ff5cd415b0f4
João Darwin <107586789+Joao-Darwin@users.noreply.github.com>
2023-01-12 12:54:27 -0300

    Criação da camada de Resource na entidade Task e implementação das querisições (#2)
    * Implementação do deleteAll no TaskService
    
    * Implementação do insert no TaskResource
    
    * Implementação do findById no TaskResource
    
    * Implementação do findAll no TaskResource
    
    * Implementação do deleteById no TaskResource
    
    * Implementação do deleteAll no TaskResource
    
    * Implementação do updateTask no TaskResource

---
+++ back-end/src/main/java/com/apirestful/apirestful/resources/TaskResource.java
@@ -0,0 +1,61 @@
+package com.apirestful.apirestful.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apirestful.apirestful.entities.Task;
+import com.apirestful.apirestful.services.TaskService;
+
+@RestController
+@RequestMapping(value = "/task")
+public class TaskResource {
+	
+	@Autowired
+	private TaskService taskService;
+	
+	@PostMapping
+	public ResponseEntity<Task> insert(@RequestBody Task task) {
+		task = taskService.insert(task);
+		return ResponseEntity.ok().body(task);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Task> findById(@PathVariable Long id) {
+		Task nTask = taskService.findById(id);
+		return ResponseEntity.ok().body(nTask);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Task>> findAll() {
+		List<Task> list = taskService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+		taskService.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<Void> deleteAll() {
+		taskService.deleteAll();
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Task> update(@PathVariable Long id, @RequestBody Task task) {
+		task = taskService.update(id, task);
+		return ResponseEntity.ok().body(task);
+	}
+}
