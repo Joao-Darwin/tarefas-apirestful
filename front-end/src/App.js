64533f85dd1ab7f6a03c5d5e25e81db1c5e197d4
João Darwin <107586789+Joao-Darwin@users.noreply.github.com>
2023-01-25 00:28:52 -0300

    Atualizações na camada de Resource no Back-End e criação do Front  (#3)
    * Inicialização do Front-End do projeto
    
    * Mudanças na estrutura do Front-End (Html, CSS e JS for REACT)
    
    * Modificação - Adição do @CrossOrigin(*) nas camadas Resources
    
    * Requisição criada
    
    * Modificações na Requisição do Front pro Back
    
    * Mudança nas pastas do front

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FormDaRequisicao from './components/FormDaRequisicao';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tarefas</h1>
+      <FormDaRequisicao/>
+    </div>
+  );
+}
+
+export default App;
